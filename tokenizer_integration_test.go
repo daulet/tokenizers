f447e46c45af5c445cc7b586f7e7219d1abac046
msama <michele@karehq.com>
2023-06-12 16:58:51 +0200

    DP-71661 integration tests

---
+++ tokenizer_integration_test.go
@@ -0,0 +1,48 @@
+package tokenizers_test
+
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/daulet/tokenizers"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type embeddingsSample struct {
+	Text            string          `json:"text"`
+	Tokens          []string        `json:"tokens"`
+	TokenizerOutput tokenizerOutput `json:"tokenizer_output"`
+	Embeddings      [][]float32     `json:"embeddings"`
+}
+
+type tokenizerOutput struct {
+	InputIDs      []uint32 `json:"input_ids"`
+	AttentionMask []uint32 `json:"attention_mask"`
+}
+
+func TestEncodeAdequacy(t *testing.T) {
+	tk, err := tokenizers.FromFile("./fixtures/tokenizer.json")
+	require.NoError(t, err)
+	defer tk.Close()
+
+	readFile, err := os.Open("./fixtures/test_embeddings.jsonl")
+	require.NoError(t, err)
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		data := fileScanner.Bytes()
+		sample := &embeddingsSample{}
+		err := json.Unmarshal(data, sample)
+		require.NoError(t, err)
+
+		actual := tk.Encode(sample.Text, true)
+		assert.Equal(t, sample.TokenizerOutput.InputIDs, actual)
+	}
+
+	readFile.Close()
+}
