b80b491b3d875d78dfebf975857c1d799a299e1f
Clément Michaud <clement.michaud34@gmail.com>
2023-07-25 19:00:45 +0200

    fix the example and the readme

--- example/main.go
+++ example/main.go
@@ -15,11 +15,9 @@ func main() {
 	defer tk.Close()
 	fmt.Println("Vocab size:", tk.VocabSize())
 	// Vocab size: 30522
-	encoding := tk.Encode("brown fox jumps over the lazy dog", false)
-	fmt.Println(encoding.IDs, encoding.Tokens)
+	fmt.Println(tk.Encode("brown fox jumps over the lazy dog", false))
 	// [2829 4419 14523 2058 1996 13971 3899] [brown fox jumps over the lazy dog]
-	encoding = tk.Encode("brown fox jumps over the lazy dog", true)
-	fmt.Println(encoding.IDs, encoding.Tokens)
+	fmt.Println(tk.Encode("brown fox jumps over the lazy dog", true))
 	// [101 2829 4419 14523 2058 1996 13971 3899 102] [[CLS] brown fox jumps over the lazy dog [SEP]]
 	fmt.Println(tk.Decode([]uint32{2829, 4419, 14523, 2058, 1996, 13971, 3899}, true))
 	// brown fox jumps over the lazy dog
